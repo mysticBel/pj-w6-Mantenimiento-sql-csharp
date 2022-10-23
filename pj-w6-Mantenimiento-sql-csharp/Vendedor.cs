f351d4450e4dea0c10a16c1ae25dbbd5d9fd1f1b
Maribel Maza <80657076+mysticBel@users.noreply.github.com>
2022-10-22 21:37:15 -0500

    agregado clase Vendedor

---
+++ pj-w6-Mantenimiento-sql-csharp/Vendedor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pj_w6_Mantenimiento_sql_csharp
+{
+    internal class Vendedor
+    {
+        public int ide_ven { get; set; }
+        public string nom_ven { get; set; }
+        public string ape_ven { get; set; }
+        public double sue_ven { get; set; }
+        public DateTime fec_ing { get; set; }
+        public int ide_dis { get; set; }
+    }
+}
