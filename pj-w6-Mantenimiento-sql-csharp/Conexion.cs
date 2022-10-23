0c46b1545b99d7cd644d406300db2007dddba128
Maribel Maza <80657076+mysticBel@users.noreply.github.com>
2022-10-22 21:35:20 -0500

    Inicializado proyecto, agregado el metodo de conexion

---
+++ pj-w6-Mantenimiento-sql-csharp/Conexion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace pj_w6_Mantenimiento_sql_csharp
+{
+    internal class Conexion
+    {
+        //Metodo de conexion 
+        public SqlConnection getConecta()
+        {
+            SqlConnection cn = new SqlConnection(ConfigurationManager
+                                          .ConnectionStrings["cn"].ConnectionString);
+            return cn;
+        }
+    }
+}
