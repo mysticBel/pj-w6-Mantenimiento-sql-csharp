85d1710a1d5e01887bdbc39589880e1a0fa665a7
Maribel Maza <80657076+mysticBel@users.noreply.github.com>
2022-10-23 16:12:40 -0500

    eliminar vendedor implementado. Fin de proyecto :)

--- pj-w6-Mantenimiento-sql-csharp/frmEliminaVendedor.Designer.cs
+++ pj-w6-Mantenimiento-sql-csharp/frmEliminaVendedor.Designer.cs
@@ -28,12 +28,233 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgVendedores = new System.Windows.Forms.DataGridView();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.cboDistrito = new System.Windows.Forms.ComboBox();
+            this.dtFecha = new System.Windows.Forms.DateTimePicker();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.txtSueldo = new System.Windows.Forms.TextBox();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgVendedores)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgVendedores
+            // 
+            this.dgVendedores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgVendedores.Location = new System.Drawing.Point(19, 344);
+            this.dgVendedores.Name = "dgVendedores";
+            this.dgVendedores.Size = new System.Drawing.Size(672, 171);
+            this.dgVendedores.TabIndex = 58;
+            this.dgVendedores.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dgVendedores_MouseDoubleClick);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.LightCoral;
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(373, 272);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(264, 47);
+            this.btnEliminar.TabIndex = 57;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(111, 96);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(91, 20);
+            this.txtCodigo.TabIndex = 56;
+            // 
+            // cboDistrito
+            // 
+            this.cboDistrito.FormattingEnabled = true;
+            this.cboDistrito.Location = new System.Drawing.Point(35, 298);
+            this.cboDistrito.Name = "cboDistrito";
+            this.cboDistrito.Size = new System.Drawing.Size(263, 21);
+            this.cboDistrito.TabIndex = 55;
+            // 
+            // dtFecha
+            // 
+            this.dtFecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtFecha.Location = new System.Drawing.Point(371, 229);
+            this.dtFecha.Name = "dtFecha";
+            this.dtFecha.Size = new System.Drawing.Size(264, 20);
+            this.dtFecha.TabIndex = 54;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(368, 169);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(268, 20);
+            this.txtApellidos.TabIndex = 51;
+            // 
+            // txtSueldo
+            // 
+            this.txtSueldo.Location = new System.Drawing.Point(35, 232);
+            this.txtSueldo.Name = "txtSueldo";
+            this.txtSueldo.Size = new System.Drawing.Size(268, 20);
+            this.txtSueldo.TabIndex = 52;
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(35, 169);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(268, 20);
+            this.txtNombres.TabIndex = 53;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(225, 84);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(139, 43);
+            this.btnBuscar.TabIndex = 50;
+            this.btnBuscar.Text = "BUSCAR";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(364, 146);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 20);
+            this.label7.TabIndex = 45;
+            this.label7.Text = "APELLIDOS";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(367, 206);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(143, 20);
+            this.label6.TabIndex = 46;
+            this.label6.Text = "FECHA INGRESO";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(31, 272);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 20);
+            this.label5.TabIndex = 47;
+            this.label5.Text = "DISTRITO";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(31, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 20);
+            this.label4.TabIndex = 48;
+            this.label4.Text = "SUELDO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(31, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 20);
+            this.label3.TabIndex = 49;
+            this.label3.Text = "NOMBRES";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 44;
+            this.label2.Text = "CODIGO";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightCoral;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(709, 50);
+            this.panel1.TabIndex = 43;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(167, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ELIMINAR  VENDEDOR";
+            // 
+            // frmEliminaVendedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(708, 527);
+            this.Controls.Add(this.dgVendedores);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.cboDistrito);
+            this.Controls.Add(this.dtFecha);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtSueldo);
+            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmEliminaVendedor";
             this.Text = "frmEliminaVendedor";
+            this.Load += new System.EventHandler(this.frmEliminaVendedor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgVendedores)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgVendedores;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.ComboBox cboDistrito;
+        private System.Windows.Forms.DateTimePicker dtFecha;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.TextBox txtSueldo;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- pj-w6-Mantenimiento-sql-csharp/frmEliminaVendedor.cs
+++ pj-w6-Mantenimiento-sql-csharp/frmEliminaVendedor.cs
@@ -12,9 +12,78 @@ namespace pj_w6_Mantenimiento_sql_csharp
 {
     public partial class frmEliminaVendedor : Form
     {
+
+        DAOVendedor objDAO = new DAOVendedor();
         public frmEliminaVendedor()
         {
             InitializeComponent();
+            cboDistrito.DataSource = objDAO.listadoDistritos();
+            cboDistrito.DisplayMember = "nom_dis";
+            cboDistrito.ValueMember = "ide_dis";
+
+            txtNombres.Enabled = false;
+            txtApellidos.Enabled = false;
+            cboDistrito.Enabled = false;
+            txtSueldo.Enabled = false;
+            dtFecha.Enabled = false;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int codigo = int.Parse(txtCodigo.Text);
+                DataRow dr = objDAO.buscaVendedor(codigo).Rows[0];
+
+                txtNombres.Text = dr[1].ToString();
+                txtApellidos.Text = dr[2].ToString();
+                txtSueldo.Text = dr[3].ToString();
+                dtFecha.Value = DateTime.Parse(dr[4].ToString());
+                cboDistrito.Text = dr[5].ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Vendedor NO existe..!!!");
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            Vendedor objV = new Vendedor()
+            {
+                ide_ven = int.Parse(txtCodigo.Text),
+            };
+            try
+            {
+                int n = objDAO.eliminaVendedor(objV);
+                if (n == 1)
+                    MessageBox.Show(n + " Registro de vendedor ELIMINADO");
+                dgVendedores.DataSource = objDAO.listadoVendedores();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+        private void frmEliminaVendedor_Load(object sender, EventArgs e)
+        {
+            dgVendedores.DataSource = objDAO.listadoVendedores();
+        }
+
+        private void dgVendedores_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int codigo = int.Parse(dgVendedores.CurrentRow.Cells[0].Value.ToString());
+
+            DataRow dr = objDAO.buscaVendedor(codigo).Rows[0];
+            txtCodigo.Text = dr[0].ToString();
+            txtNombres.Text = dr[1].ToString();
+            txtApellidos.Text = dr[2].ToString();
+            txtSueldo.Text = dr[3].ToString();
+            dtFecha.Value = DateTime.Parse(dr[4].ToString());
+            cboDistrito.Text = dr[5].ToString();
         }
     }
 }
